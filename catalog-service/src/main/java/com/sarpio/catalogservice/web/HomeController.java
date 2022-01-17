3d97e1cd5713dc1af2ec7ca0bb64001210ec1ae5
sarpio <kontakt@piotrsarnecki.com>
2022-01-17 13:07:01 +0100

    Configuration Start

--- catalog-service/src/main/java/com/sarpio/catalogservice/web/HomeController.java
+++ catalog-service/src/main/java/com/sarpio/catalogservice/web/HomeController.java
@@ -1,18 +1,17 @@
 package com.sarpio.catalogservice.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
-
 public class HomeController {
 
     @Value("${polar.greeting}")
     private String greetings;
 
+    @RefreshScope
     @GetMapping("/")
     public String getGreeting() {
         return greetings;
